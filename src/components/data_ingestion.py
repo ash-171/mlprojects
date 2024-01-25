457dfcef0b36b1e87d4ee322c895cc14271d3885
Aswathi <80941531+ash-171@users.noreply.github.com>
2024-01-25 12:32:23 +0530

    Update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -44,8 +44,8 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e,sys)
-'''
+
 if __name__=='__main__':
     data_ingestion=DataIngestion()
     data_ingestion.initiate_data_ingestion()         
-'''
+
